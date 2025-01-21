fd19824a4071d6ae9665a064cfb2710caa0b0b71
tom <tom.makni@gmail.com>
2025-01-21 12:24:37 +0100

    ok

--- python-data_structures/4-main.py
+++ python-data_structures/4-main.py
@@ -4,9 +4,9 @@ def new_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
         my_list[idx] = element
 
+
 new_list = [1, 2, 3, 9, 5]
 print(new_list)
 
 new_in_list(new_list, 3, 4)
 print(new_list)
-
