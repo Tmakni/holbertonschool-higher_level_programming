21aa51c5ca62b37388407cc93f91fcccf75a52a5
tom <tom.makni@gmail.com>
2025-01-22 09:54:21 +0100

    ok

--- python-data_structures/4-main.py
+++ python-data_structures/4-main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+new_in_list = __import__('4-new_in_list').new_in_list
 
-def new_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
-        my_list[idx] = element
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
 
-
-new_list = [1, 2, 3, 9, 5]
 print(new_list)
-
-new_in_list(new_list, 3, 4)
-print(new_list)
+print(my_list)
--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for item in my_list:
-        print("{:d}".format(item))
-        print("{:d}".format(item))
+
+
+def new_in_list(my_list, idx, element):
+    """
+    Replaces an element in a list at a specific position
+    Without modifying the original list
+    """
+    l_len = len(my_list)
+    if idx >= l_len or idx < 0:
+        return (my_list)
+
+    new_list = my_list[:]
+    new_list[idx] = element
+    return (new_list)
