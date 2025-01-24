ffe613b99db3310163dc7aadb18d8d360230236b
tom <tom.makni@gmail.com>
2025-01-24 11:11:52 +0100

    ok

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    count = 0  # Compteur pour le nombre d'éléments imprimés
-    for i in range(x):
-        try:
-            print(my_list[i], end="")  # Imprime l'élément sans nouvelle ligne
-            count += 1  # Incrémente le compteur
-        except IndexError:
-            break  # Arrête la boucle si l'indice est hors de porté
-    print()  # Imprime une nouvelle ligne à la fin
-    return count  # Retourne le nombre d'éléments imprimés
+
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print("")
+    return count
