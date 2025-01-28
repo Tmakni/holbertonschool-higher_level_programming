5ca65db9aec66351afa8215411822bfdf5d17135
tom <tom.makni@gmail.com>
2025-01-28 11:06:25 +0100

    ok

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# 1-square.py
+
 
 class Square:
+
 
     def __init__(self, size):
         self.size = size
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
+
 class Square:
     def __init__(self, size=0):
         """Initialise un carré avec une taille optionnelle."""
         self.size = size  # Appelle le setter pour valider et initialiser la taille
 
+
     @property
     def size(self):
         """Getter pour récupérer la taille du carré."""
         return self._size
+
 
     @size.setter
     def size(self, value):
@@ -18,6 +21,7 @@ class Square:
         if value < 0:  # Vérifie si la taille est positive ou nulle
             raise ValueError("size must be >= 0")
         self._size = value  # Assigne la taille validée
+
 
     def area(self):
         """Retourne l'aire du carré."""
