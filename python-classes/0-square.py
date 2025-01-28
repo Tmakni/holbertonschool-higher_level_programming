080adb1e35ce3477db52345617850bff191bfc4a
tom <tom.makni@gmail.com>
2025-01-28 11:11:43 +0100

    ok

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Module 0-square.py
+Provides an empty class Square that defines a square
+"""
 
 
 class Square:
+    """
+    Provides an empty class Square that defines a square
+    """
 
     pass
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+Module 2-square.py
+Provides an empty class Square that defines a square
+"""
+
 
 class Square:
+    """
+    Provides an empty class Square that defines a square
+
+    Attribute :
+        Size (private) of the square
+    """
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
