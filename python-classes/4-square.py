05d7809b70a43e7bcc70b7446c8d3073288c1144
tom <tom.makni@gmail.com>
2025-01-28 11:03:03 +0100

    ok

---
+++ python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        """Initialise un carré avec une taille optionnelle."""
+        self.size = size  # Appelle le setter pour valider et initialiser la taille
+
+    @property
+    def size(self):
+        """Getter pour récupérer la taille du carré."""
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """Setter pour valider et définir la taille du carré."""
+        if not isinstance(value, int):  # Vérifie si la taille est un entier
+            raise TypeError("size must be an integer")
+        if value < 0:  # Vérifie si la taille est positive ou nulle
+            raise ValueError("size must be >= 0")
+        self._size = value  # Assigne la taille validée
+
+    def area(self):
+        """Retourne l'aire du carré."""
+        return self._size ** 2  # Calcul de l'aire (côté au carré)
