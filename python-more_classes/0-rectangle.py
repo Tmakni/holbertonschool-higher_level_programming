8978ed394c3a89b80aaf3d3c531f9e40deba2273
tom <tom.makni@gmail.com>
2025-01-31 11:13:22 +0100

    ok

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -6,4 +6,5 @@ Provides an empty class Square that defines a square """
 class Rectangle:
     """Provides an empty class Rectangle that defines a rectangle"""
     def __init__(self):
-    pass
+
+        pass
