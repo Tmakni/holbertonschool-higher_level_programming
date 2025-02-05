c3504f57a5c38b4943d0e2c6ab07c50b1de4f294
tom <tom.makni@gmail.com>
2025-02-05 11:13:14 +0100

    ok

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list.py
+Provides a class MyList that inherits from list
+"""
+
+
 class MyList(list):
-    """Classe qui hérite de list et ajoute une méthode pour afficher une version triée de la liste."""
-    
+    """
+    Provides a class MyList that inherits from list
+    """
     def print_sorted(self):
-        """Affiche la liste triée sans modifier l'originale."""
+        """
+        Print a sorted list of int
+        """
         print(sorted(self))
