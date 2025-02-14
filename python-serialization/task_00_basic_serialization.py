a0e18aeb0036138449cc2c7e93d292a5a2239904
tom <tom.makni@gmail.com>
2025-02-14 11:45:15 +0100

    ok

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,9 +5,10 @@ Function
 
 
 def serialize_and_save_to_file(data, filename):
-     with open(filename, 'w') as file:
+    with open(filename, 'w') as file:
         json.dump(data, file)
     pass
+
 
 def load_and_deserialize(filename):
     with open(filename, 'r') as file:
