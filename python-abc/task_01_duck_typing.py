e3d8f9c97fed4e1411fa7ec23558c49c01dee148
tom <tom.makni@gmail.com>
2025-02-26 10:20:33 +0100

    ok

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -34,3 +34,7 @@ class Rectangle(Shape):
 
     def perimeter(self):
         return 2 * (self.width + self.height)
+
+    def shape_info(shape):
+        print(f"Area: {shape.area()}")
+        print(f"Perimeter: {shape.perimeter()}")
