0c0a147e2a7cc8a53af5566b9f523dd9fc8e39d6
tom <tom.makni@gmail.com>
2025-02-27 13:36:49 +0100

    ok

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,8 @@ Fonction
 
 
 def append_write(filename="", text=""):
+    """
+    write a text file and return its content to stdout
+    """
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
