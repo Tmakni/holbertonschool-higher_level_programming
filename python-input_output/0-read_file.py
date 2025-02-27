ac670af22d5b1266577c40e1d53bfde4eafb341a
tom <tom.makni@gmail.com>
2025-02-27 11:30:12 +0100

    ok

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,5 +2,8 @@
 
 
 def read_file(filename=""):
+    """
+    Fonction pour ecrire tout les caractères avec utf-8
+    """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
