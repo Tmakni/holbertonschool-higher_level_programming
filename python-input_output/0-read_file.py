5beedfce6e96e99275f208eecf7a99b250783cc8
tom <tom.makni@gmail.com>
2025-02-27 11:50:46 +0100

    ok

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,8 +2,6 @@
 
 
 def read_file(filename=""):
-    """
-    Fonction qui lit un fichier texte (en encodage UTF-8) et affiche
-    """
+    """ Fonction qui lit un fichier texte (en encodage UTF-8) et affiche """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
