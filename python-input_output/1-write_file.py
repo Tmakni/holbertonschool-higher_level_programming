53c91be2016e2385ba7b99a3ff9ba2becc98b8c9
tom <tom.makni@gmail.com>
2025-02-27 12:54:20 +0100

    ok

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,8 @@ Fonction
 
 
 def write_file(filename="", text=""):
+    """
+    write a text file and return its content to stdout
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
