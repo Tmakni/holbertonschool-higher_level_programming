0e4ed9e700fd5412aea678f6ebeaa38526e8aff7
tom <tom.makni@gmail.com>
2025-02-27 12:32:56 +0100

    ok

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
-
-def read_file(filename=""):
     """
-    Reads a text file and prints its content to stdout.
+    Fonction
+    """
 
-    :param filename: The path to the file (defaults to an empty string).
+def read_file(filename="")
+    """
+    Reads a text file and prints its content to stdout
+
+    param filename: The path to the file (defaults to an empty string)
     """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
