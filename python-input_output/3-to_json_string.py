11eb572ac0533d2228e959fd4764e56fe5cae305
tom <tom.makni@gmail.com>
2025-02-27 13:56:12 +0100

    ok

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+Fonction
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Retourne la représentation JSON d'un objet Python.
+    """
+    return json.dumps(my_obj)
