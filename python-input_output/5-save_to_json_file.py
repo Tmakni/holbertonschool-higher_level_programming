62c733172d41ed19ade6fa8a558335dfcb3502b6
tom <tom.makni@gmail.com>
2025-02-28 10:02:25 +0100

    ok

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,14 @@
-sqd
+#!/usr/bin/python3
+"""
+Fonction
+"""
+import json
+""" import json"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Ecris directement dans le fichier.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return json.dump(my_obj, file)
