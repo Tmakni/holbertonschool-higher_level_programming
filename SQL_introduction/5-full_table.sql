74ee84380d0c52774b929917cf14c9db3905be75
tom <tom.makni@gmail.com>
2025-03-04 14:30:59 +0100

    ok

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 -- script that prints the following description of the table
-Table Create Table
+Table         Create Table
 first_table   CREATE TABLE `first_table` (
-        `id` int DEFAULT NULL,
-        `name` varchar(256) DEFAULT NULL,
-);
+                `id` int DEFAULT NULL,
+                `name` varchar(256) DEFAULT NULL
+              ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
