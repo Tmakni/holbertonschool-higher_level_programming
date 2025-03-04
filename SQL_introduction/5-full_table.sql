4187c3bbe9147a84fadb4c7fa466da3a53f78945
tom <tom.makni@gmail.com>
2025-03-04 14:26:16 +0100

    ok

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- script that prints the following description of the table
+Table Create Table
+first_table   CREATE TABLE `first_table` (
+        `id` int DEFAULT NULL,
+        `name` varchar(256) DEFAULT NULL,
+);
