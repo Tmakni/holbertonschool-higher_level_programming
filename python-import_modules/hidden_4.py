acad39f7f6a17e509e531579277cf061cbd57392
Tmakni <tom.makni@gmail.com>
2025-05-07 22:42:36 +0200

    ok

---
+++ python-import_modules/hidden_4.py
@@ -0,0 +1,13 @@
+# hidden_4.py
+
+my_secret_santa = "John"
+print_hidden = "This is hidden"
+print_school = "This is the school"
+
+def hello():
+    print("Hello from the function!")
+
+def print_message():
+    print("This is another function")
+
+__hidden_var = "This should not be displayed"
