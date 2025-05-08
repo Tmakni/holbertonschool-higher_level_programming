c5ac7348aa8dc79c677509420e7f05d02a7d71e2
Tmakni <tom.makni@gmail.com>
2025-05-08 12:31:03 +0200

    ok

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx <= len(my_list):
-        copie = my_list
-        copie[3] = 9
-        return(my_list)
+        copie = list(my_list)
+        copie[idx] = element
+        return(copie)
