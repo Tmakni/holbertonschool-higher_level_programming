87498e23891c1d028759fa29583abe2abd0bf293
Tmakni <tom.makni@gmail.com>
2025-05-08 12:44:28 +0200

    sauv

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+def no_c(my_string):
+    for lettre in my_string:
+        if lettre == 'c' or lettre == 'C':
+            pass
+        new_string = 'c, C'
+    for lettre in new_string:
+        pass
+    return my_string
