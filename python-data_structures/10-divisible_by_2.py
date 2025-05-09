880860f046abe5fa50ee9b18adc98866dc3bc073
Tmakni <tom.makni@gmail.com>
2025-05-09 11:56:40 +0200

    ok

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    resultats = []
+    for number in my_list:
+        if number % 2 == 0:
+            resultats.append(True)
+        else:
+            resultats.append(False)
+    return resultats
