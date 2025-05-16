cca5e7cb773d84cf5090d44f143a446a7c2f42c3
Tmakni <tom.makni@gmail.com>
2025-05-16 14:59:06 +0200

    ok

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,6 @@
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        num1 = my_list_1(i)
+        num2 = my_list_2(i)
+        return list_length
