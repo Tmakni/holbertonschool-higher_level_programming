d0e7d7e75e21e8dd214992711a83c667b7273900
Tmakni <tom.makni@gmail.com>
2025-05-19 09:56:16 +0200

    docstring

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,14 @@
+#!/usr/bin/python3
+"""Module defining a Square class."""
+
 class Square:
+    """
+       class square
+    """
+
     def __init__(self, size=0):
+        """Initialize a new Square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
