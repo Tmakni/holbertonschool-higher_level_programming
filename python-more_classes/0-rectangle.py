892d0d230a6e8ff16f76d4d4449208427ac89702
Tmakni <tom.makni@gmail.com>
2025-05-19 12:32:09 +0200

    ok

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """class Rectangle"""
 
-
 class Rectangle:
-    """pass"""
+    """
+    Classe Rectangle vide.
+    Cette classe définit un rectangle, mais sans attributs ni méthodes pour l'instant.
+    """
     def __init__(self):
         pass
