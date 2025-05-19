0387878b770805145df9d2c9214c6a50bd4a2d62
Tmakni <tom.makni@gmail.com>
2025-05-19 09:57:17 +0200

    docstring

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """Initialize the square with size."""
         self.__size = size
