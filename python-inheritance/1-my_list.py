58c7a7006ee893b451f90f39509506a26aaabff0
Tmakni <tom.makni@gmail.com>
2025-05-27 10:21:49 +0200

    ok

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+class my list
+"""
+
+
+class MyList(list):
+    """
+    class mylist
+    """
+
+    def print_sorted(self):
+        """
+        print_sorted
+        """
+        print(sorted(self))
