c82d7e0b3f5666c6dd20ab28e35f0bf04ee98cc2
Tmakni <tom.makni@gmail.com>
2025-06-02 11:48:51 +0200

    ok

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,5 +3,8 @@
 
 
 def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints its contents to stdout.
+    """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
