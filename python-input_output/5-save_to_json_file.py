1fdaef664eae55b2ef25e6435aaea9c98ac3cb03
Tmakni <tom.makni@gmail.com>
2025-06-02 12:25:17 +0200

    ok

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,6 +1,14 @@
+#!/usr/bin/python3
+"""json import"""
+
+
 import json
 
+
 def save_to_json_file(my_obj, filename):
+    """
+    wirte file and return json.dumps
+    """
     with open(filename, "w", encoding="utf-8")as file:
         my_obj = json.dumps(my_obj)
         return my_obj
