1351723babe22666a36edd7c1d301505648c6bc2
Tmakni <tom.makni@gmail.com>
2025-06-02 12:01:42 +0200

    ok

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ def write_file(filename="", text=""):
     write a text file
     """
     with open(filename, "w", encoding="utf-8")as file:
-        strw = file.write(filename)
+        strw = file.write(text)
         return(strw)
