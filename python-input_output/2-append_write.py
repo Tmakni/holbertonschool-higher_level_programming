032a07c5e02ac865435179cca417e0c9c51a5748
Tmakni <tom.makni@gmail.com>
2025-06-02 12:10:12 +0200

    ok

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,6 +5,6 @@ def append_write(filename="", text=""):
     """
     write a text file
     """
-    with open(filename, "a", opening="utf-8")as file:
+    with open(filename, "a", encoding="utf-8")as file:
         strw = file.write(text)
         return (strw)
