4a3ea0e43e140f82d7840a5c3aa0f1af782c17ac
Tmakni <tom.makni@gmail.com>
2025-06-02 12:34:55 +0200

    ok

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""json import"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    wirte file et lit depuis le fichier ouvert(json.load)
+    """
+    with open(filename, "w", encoding="utf-8")as file:
+        return json.load(file)
