1bab7ac5eeddc2de92f84568ea0c051215f30d65
Tmakni <tom.makni@gmail.com>
2025-06-12 14:37:19 +0200

    ok

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
 """
 Ceci est une API simple construite avec http.server et socketserver.
-Elle définit plusieurs endpoints pour renvoyer du texte ou du JSON,
-puis lance un serveur HTTP et exécute des tests automatiques.
 """
-import http.server
-import socketserver
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+"""
+Ceci est une API simple construite avec http.server et socketserver.
+"""
 
 
 class http_SubClass(http.server.BaseHTTPRequestHandler):
     """
     Gestionnaire de requêtes HTTP GET pour notre API simple.
-    Hérite de BaseHTTPRequestHandler et redéfinit do_GET pour traiter
-    différents chemins d'URL et renvoyer les réponses appropriées.
     """
     def do_GET(self):
         """
-        Traitement des requêtes GET :
-        '/'       : renvoie un message texte "Hello, this is a simple API!"
-        '/data'   : renvoie un JSON avec des données d'exemple
-        '/info'   : renvoie un JSON avec la version et une description
-        '/status' : renvoie un simple message texte "OK"
-        autre     : renvoie un code 404 et un message d'erreur
+        Traitement des requêtes GET
         """
         if self.path == '/':
             self.send_response(200)
