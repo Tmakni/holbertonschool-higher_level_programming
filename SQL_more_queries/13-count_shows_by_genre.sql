d85707264d999af9e29591a7ac185b4d1c094d49
Tmakni <tom.makni@gmail.com>
2025-06-26 15:42:08 +0200

    ok

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- List User
-SELECT cities.id,
-       cities.name,
-       states.name
-FROM   cities
-JOIN   states ON states.id = cities.state_id
-ORDER  BY cities.id ASC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
