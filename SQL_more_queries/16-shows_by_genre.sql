bb4dc2bff26477d97ce6d7e80cbd6b6167478c6e
Tmakni <tom.makni@gmail.com>
2025-06-27 10:10:56 +0200

    ok

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- List User
-SELECT ts.title
+SELECT  ts.title,
         tg.name
-FROM tv_shows       AS ts
-LEFT JOIN tv_show_genres AS sg ON sg.show_id = ts.id
-LEFT JOIN tv_genres     AS tg ON tg.id      = ts.id
+FROM    tv_shows         AS ts
+LEFT JOIN tv_show_genres AS sg  ON sg.show_id  = ts.id
+LEFT JOIN tv_genres      AS tg  ON tg.id       = sg.genre_id
 ORDER BY ts.title ASC, tg.name ASC;
