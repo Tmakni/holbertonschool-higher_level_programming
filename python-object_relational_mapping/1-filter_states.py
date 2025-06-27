725051cb5a604fb805280832d4b8119616711108
Tmakni <tom.makni@gmail.com>
2025-06-27 13:44:53 +0200

    ok

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Lists all states starting with 'N' from hbtn_0e_0_usa (ordered by id ASC).
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    user, pwd, db = sys.argv[1:4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=pwd,
+        db=db,
+        charset="utf8"
+    )
+
+    cur = conn.cursor()
+
+    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    cur.execute(sql)
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    conn.close()
